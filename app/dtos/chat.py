b18cd7ac35e014f71da7139174cdbb9a05257273
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2024-07-20 19:41:08 +0900

    refactor: chat to summarize dto 로 수정

--- app/dtos/chat.py
+++
@@ -1,7 +0,0 @@
-class ChatDto:
-    def __init__(self,message: str):
-        self._message = message
-
-    @property
-    def message(self):
-        return self._message
---
+++ app/dtos/summarize.py
@@ -0,0 +1,9 @@
+class RssDto:
+    def __init__(self, id: str):
+        self._id = id
+
+    @property
+    def id(self):
+        return self._id
+
+
