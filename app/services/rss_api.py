6e3247e84e908f1fd4b83e3c7a3416927aa02c12
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2024-07-22 15:13:56 +0900

    fix: 버그 해결

--- app/services/rss_api.py
+++ app/services/rss_api.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import List
+from typing import List, Optional
 
 import feedparser
 from flask_sqlalchemy import SQLAlchemy
@@ -8,19 +8,22 @@ from app.models import RSS
 
 
 class RssService:
-    def __init__(self, db: SQLAlchemy):
+    rss_s: Optional[List[RSS]]
+
+    def __init__(self, db: SQLAlchemy, rss_s=None):
         self.db = db
+        self.rss_s = rss_s
 
     def parse_rss(self, rss_link:str) -> List[RSS]:
         news_feed = feedparser.parse(rss_link)
 
-        contents = [RSS(id=content.link,title=content.title,content=content.content[0].value) for
-                    content in news_feed.entries]
+        self.rss_s = self.db.session.execute(self.db.select(RSS).filter_by(id=rss_link)).all()
+        if self.rss_s:
+            return self.rss_s
 
-        self.db.session.add_all(contents)
+        self.rss_s = [RSS(id=content.link,title=content.title,content=content.content[0].value) for content in news_feed.entries]
 
-        return contents
+        self.db.session.add_all(self.rss_s)
 
-    # def push_summary(self, rss_link:str) -> str:
-    #     rss = self.db.session.execute(self.db.select(RSS).filter(RSS.link == rss_link).first())
-    #     rss.su
+        return self.rss_s
+
