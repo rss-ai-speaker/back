5bef55b671956e69d987e0cd028035b55027b821
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2024-07-22 15:14:19 +0900

    feat: Content 스키마 추가

--- app/models.py
+++ app/models.py
@@ -19,16 +19,16 @@ class RSS(db.Model):
         db.session.commit()
 
 
-# class Content(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     rss_id = db.Column(db.Integer, db.ForeignKey("rss.id"), nullable=False)
-#     summary = db.Column(db.String(120), nullable=False)
-#     created_at = db.Column(db.DateTime, nullable=False)
-#
-#     def __init__(self,rss_id:int,summary:str,created_at:Optional[datetime]=None):
-#         self.rss_id = rss_id
-#         self.summary = summary
-#         self.created_at = created_at if created_at else datetime.now()
-#         db.session.add(self)
-#         db.session.commit()
+class Content(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rss_id = db.Column(db.String, db.ForeignKey("rss.id"), nullable=False)
+    summary = db.Column(db.String(120), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self,rss_id:int,summary:str,created_at:Optional[datetime]= None):
+        self.rss_id = rss_id
+        self.summary = summary
+        self.created_at = created_at if created_at else datetime.now()
+        db.session.add(self)
+        db.session.commit()
 
