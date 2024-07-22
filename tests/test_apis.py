39f665b1424da254579e1ddd4b287ccf13b2fb27
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2024-07-22 15:13:08 +0900

    feat: test 추가

--- tests/test_apis.py
+++ tests/test_apis.py
@@ -1,6 +1,7 @@
+import json
+
 import pytest
 from app import create_app
-
 
 @pytest.fixture
 def client():
@@ -11,5 +12,13 @@ def client():
 
 
 def test_summarize(client):
-    response = client.get('/api/summarize')
+    response = client.get('/api/summarize?id=https://www.reddit.com/r/Python/comments/wzswh9/is_flask_even_being_used_nowdays/.rss')
     assert len(response.text) > 100
+
+def test_post_summarize(client):
+    data = {
+        "link":"https://www.reddit.com/r/reinforcementlearning/.rss"
+    }
+    response = client.post('/api/summarize',data=json.dumps(data),
+                headers={"Content-Type": "application/json"})
+    assert response.status_code == 201
