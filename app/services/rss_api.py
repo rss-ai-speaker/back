61c60c26b0504b54e2859f6d1765a6518b732d38
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2024-07-29 21:14:01 +0900

    feat: docker init

--- app/services/rss_api.py
+++ app/services/rss_api.py
@@ -5,7 +5,6 @@ import feedparser
 from flask_sqlalchemy import SQLAlchemy
 
 from app.models import RSS, Content
-from utils.custom_logger import c_logger
 
 
 class RssService:
@@ -15,14 +14,16 @@ class RssService:
         self.db = db
         self.rss_s = rss_s
 
-    def parse_rss(self, rss_link:str) -> List[RSS]:
+    def parse_rss(self, rss_link: str) -> List[RSS]:
         news_feed = feedparser.parse(rss_link)
 
-        self.rss_s = self.db.session.execute(self.db.select(RSS).filter_by(link=rss_link)).all()
+        self.rss_s = self.db.session.execute(
+            self.db.select(RSS).filter_by(link=rss_link)).all()
         if self.rss_s:
             return [rss[0] for rss in self.rss_s]
 
-        self.rss_s = [RSS(link=rss_link,id=content.link,title=content.title,content=content.content[0].value) for content in news_feed.entries]
+        self.rss_s = [RSS(link=rss_link, id=content.link, title=content.title,
+                          content=content.content[0].value) for content in news_feed.entries]
 
         self.db.session.add_all(self.rss_s)
 
@@ -32,8 +33,8 @@ class RssService:
         contents = self.db.session.query(Content).all()
         return contents
 
-    def get_rss_content(self, content_id:str) -> Content:
-        content = self.db.session.query(Content).filter_by(id=content_id).first()
+    def get_rss_content(self, content_id: str) -> Content:
+        content = self.db.session.query(
+            Content).filter_by(id=content_id).first()
 
         return content
-
